5fe4f8e9590aca63a12d6dda0167b3b26a149083
Felix Cusson <60357894+fcusson@users.noreply.github.com>
2025-01-22 12:32:11 +0000

    Bump version: 0.1.0-a1 → 1.0.0-a0

--- src/aws_sig/__init__.py
+++ src/aws_sig/__init__.py
@@ -1,6 +1,6 @@
 """AWS Signature Manager. Provides Requests compliant Authenticator
 objects for API requests"""
 
-__version__ = "0.1.0-a1"
+__version__ = "1.0.0-a0"
 
 from aws_sig.v4 import SigV4
